5a6e5270423f112fde2458f77a791e79c4c3fbcd
minseo  jo <liging12@naver.com>
2023-09-16 04:22:48 +0900

    feat: 크롤링한 데이터를 엑셀로 변환

--- programmersJobInformation/src/main/java/info_to_excel_serial/Main.java
+++ programmersJobInformation/src/main/java/info_to_excel_serial/Main.java
@@ -1,9 +1,11 @@
+package info_to_excel_serial;
+
 public class Main {
     // 드라이버 설치 경로
     private static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
     private static final String WEB_DRIVER_PATH = "/Users/minseojo/Desktop/chromedriver-mac-arm64/chromedriver";
     public static void main(String[] args) {
-        JobInformationCrawler crawler = new JobInformationCrawler(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+        Crawler crawler = new Crawler(WEB_DRIVER_ID, WEB_DRIVER_PATH);
         crawler.exe();
     }
 }
