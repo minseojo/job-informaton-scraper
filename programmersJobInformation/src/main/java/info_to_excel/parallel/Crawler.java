e811750014b1de1097d0c3b805265e58421cb073
minseo  jo <liging12@naver.com>
2023-09-19 13:35:02 +0900

    chore: 패키지 변경

--- programmersJobInformation/src/main/java/info_to_excel/parallel/Crawler.java
+++ programmersJobInformation/src/main/java/info_to_excel/parallel/Crawler.java
@@ -1,4 +1,4 @@
-package info_to_excel_serial;
+package info_to_excel.parallel;
 
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -11,53 +11,72 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.io.FileOutputStream;
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Crawler {
+    private final int numberOfThreads;
+    private static final ChromeOptions options = new ChromeOptions();
 
-    private final WebDriver driver; // 웹드라이버
-    private final ChromeOptions options; // 웹드라이버 옵션
-    private final WebDriverWait wait; // 타이머
-    private final Workbook workbook; // 엑셀 생성 객체
-    private final Sheet sheet; // 액셀 컨트롤 객체
-    public Crawler(String WEB_DRIVER_ID, String WEB_DRIVER_PATH){
-        // 웹드라이버 경로 설정
+    public Crawler(String WEB_DRIVER_ID, String WEB_DRIVER_PATH, int numberOfThreads) {
         System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
-        // 웹드라이버 옵션 설정
-        options = new ChromeOptions();
-        options.addArguments("headless"); // 크롬 브라우저 창 시각적으로 안보이게 하기 -> 속도 빨라짐
-        options.addArguments("--start-maximized"); // 브라우저 창 최대화 -> 웹페이지 내용을 많이 표시 -> 스크래핑 속도 빨라짐
-        // 액셀 생성
-        workbook = new XSSFWorkbook();
-        sheet = workbook.createSheet("Programmers Job Information");
-        // 드라이버에 옵션 넣기 (-> 속도 빨라짐)
-        driver = new ChromeDriver(options);
-        // 드라이버가 웹페이지 접속 대기 객체
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); //웹피이지 최대 10초 기다림, 로딩완료시 바로 실행
+        this.numberOfThreads = numberOfThreads;
+        options.addArguments("headless");
+        options.addArguments("--start-maximized");
     }
 
     public void exe() {
-        // 시간 측정시작
         long startTime = System.currentTimeMillis();
         try {
-            // 크롤링할 사이트 마지막 페이지 찾기
+            ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+
+            for (int thread = 0; thread < numberOfThreads; thread++) {
+                final int currentThread = thread;
+                Callable<Void> task = () -> {
+                    Workbook workbook = new XSSFWorkbook();
+                    Sheet sheet = workbook.createSheet("Programmers Job Information");
+                    WebDriver driver = new ChromeDriver(options);
+                    crawlPage(currentThread, driver, workbook, sheet);
+                    return null;
+                };
+                executorService.submit(task);
+            }
+
+            executorService.shutdown();
+            executorService.awaitTermination(5, TimeUnit.MINUTES);
+        } catch (Exception error) {
+            error.printStackTrace();
+        } finally {
+            long endTime = System.currentTimeMillis();
+            long elapsedTimeMillis = endTime - startTime;
+            System.out.println("경과 시간 (밀리초): " + elapsedTimeMillis);
+        }
+    }
+
+    private void crawlPage(int threadNum, WebDriver driver, Workbook workbook, Sheet sheet) {
+        try {
             int lastPage = findLastPage();
-            // 헤더 행 생성
+            int totalPage = lastPage / numberOfThreads; // 각 스레드가 담당할 전체 페이지 (마지막 페이지/ 스레드개수)
+            int startPage = threadNum * totalPage; // 시작 페이지 = 전체 페이지 * 스레드 번호
+            int endPage = (threadNum + 1) * totalPage; // 마지막 페이지
             int rowNum =0;
-            for (int page = 1; page <= lastPage; page++) {
-                // 프로그래머스 채용 URL 가져와서, 소프트웨어 드라이버에 연결
+            for (int page = startPage; page < endPage; page++) {
                 String url = "https://career.programmers.co.kr/job?page=" + page;
                 driver.get(url);
-                // 한 페이지의 채용 정보를 리스트로 묶어서 한번에 가져온다.
-                WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("list-positions")));
-                // 한번에 가져온 채용 정보 리스트에 분류한다.
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("list-positions")));
                 List<WebElement> elements = element.findElements(By.className("list-position-item"));
-                // 분류된 정보를 (회사명, 회사지역, 채용분야, 채용링크) 등의 타입으로 나눠 엑셀에 저장한다.
+
                 for (WebElement e : elements) {
-                    String information = e.getText(); // 분류된 정보 (회사명, 지역, 채용분야) 정보 가져오기
-                    String link = e.findElement(By.className("position-link")).getAttribute("href"); //채용 링크 가져오기
+                    String information = e.getText();
+                    String link = e.findElement(By.className("position-link")).getAttribute("href");
+                    //   System.out.println(information + "\n" + link + "\n");
                     Row row = sheet.createRow(rowNum++); // 열 생성
                     String type[] = information.split("\n"); //회사명, 지역, 채용분야, 채용 링크 분류 (엑셀 열을 나누기 위해)
                     for (int column = 0; column < type.length; column++) {
@@ -66,23 +85,20 @@ public class Crawler {
                     row.createCell(6).setCellValue(link); // 회사 링크 6행에 저장
                 }
                 // 실제 엑셀에 쓰기
-                try (FileOutputStream outputStream = new FileOutputStream("크롤링_데이터.xlsx")) {
+                try (FileOutputStream outputStream = new FileOutputStream("크롤링_데이터" + startPage + ".xlsx")) {
                     workbook.write(outputStream);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            driver.close(); // 브라우저 종료
-            long endTime = System.currentTimeMillis();
-            long elapsedTimeMillis = endTime - startTime;
-            System.out.println("경과 시간 (밀리초): " + elapsedTimeMillis);
+            driver.quit();
         }
     }
-
-    private int findLastPage() {
+    private int findLastPage(){
         String url = "https://career.programmers.co.kr/job";
         WebDriver driver = new ChromeDriver(options);
         driver.get(url);
@@ -101,4 +117,3 @@ public class Crawler {
         return lastPage; //한 페이지당 20개
     }
 }
-
--- programmersJobInformation/src/main/java/info_to_excel/parallel/Main.java
+++ programmersJobInformation/src/main/java/info_to_excel/parallel/Main.java
@@ -1,11 +1,12 @@
-package info_to_excel_serial;
+package info_to_excel.parallel;
 
 public class Main {
     // 드라이버 설치 경로
     private static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
     private static final String WEB_DRIVER_PATH = "/Users/minseojo/Desktop/chromedriver-mac-arm64/chromedriver";
     public static void main(String[] args) {
-        Crawler crawler = new Crawler(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+        int numberOfThreads = Runtime.getRuntime().availableProcessors(); // m1 air 기준 8개
+        Crawler crawler = new Crawler(WEB_DRIVER_ID, WEB_DRIVER_PATH, numberOfThreads);
         crawler.exe();
     }
 }
