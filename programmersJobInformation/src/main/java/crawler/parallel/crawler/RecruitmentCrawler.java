83746f09894e365fbda10f36322e773c1d9bdaf5
minseo  jo <liging12@naver.com>
2023-11-26 16:31:10 +0900

    chore: 필요없는 출력문 제거

--- programmersJobInformation/src/main/java/crawler/parallel/crawler/RecruitmentCrawler.java
+++ programmersJobInformation/src/main/java/crawler/parallel/crawler/RecruitmentCrawler.java
@@ -39,7 +39,6 @@ public class RecruitmentCrawler {
     }
 
     public void execute() {
-        System.out.println(appendQuery);
         long startTime = System.currentTimeMillis();
         int totalPage = findTotalPage();
         splitPage(totalPage);
